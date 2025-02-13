dcef22a666a99fdfdf3622ab0e7fe11ec483e1d3
loonghfut <165535596+loonghfut@users.noreply.github.com>
2025-02-13 18:03:16 +0800

    图片压缩基本完成

--- src/ImageCompression/module-imageCompression.ts
+++ src/ImageCompression/module-imageCompression.ts
@@ -1,7 +1,6 @@
 import steveTools from "@/index";
 import * as api from "@/api"
 import { IProtyle, Protyle, showMessage } from "siyuan";
-// import * as ic from "@/icon"
 import imageCompression from 'browser-image-compression';
 declare const siyuan: any;
 
@@ -12,12 +11,13 @@ export class M_imageCompression {
     }
 
     public M_image_protyle:IProtyle;
+
     // 压缩单个图片
     private async compressImage(imageFile: File) {
         const options = {
-            maxSizeMB: 1,          // 最大文件大小
-            maxWidthOrHeight: 1920, // 最大宽度/高度
-            useWebWorker: true      // 使用 Web Worker 加速
+            maxSizeMB: 1,          
+            maxWidthOrHeight: 1920, 
+            useWebWorker: true      
         };
         
         try {
@@ -31,8 +31,70 @@ export class M_imageCompression {
         }
     }
 
-    init(){
+
+
+    // 打开文件选择对话框
+    private openFileDialog() {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.multiple = true;
+        input.onchange = async (e) => {
+            const files = (e.target as HTMLInputElement).files;
+            if (!files) return;
+
+            for (const file of Array.from(files)) {
+                const compressedFile = await this.compressImage(file);
+                if (compressedFile) {
+                    try {
+                        const response = await api.upload("assets/st_image", [compressedFile]);
+                        if (response.succMap) {
+                           console.log(response);
+                            let imageId = response.succMap["blob"];
+                            //去掉imageId的data/前缀
+                            imageId = imageId.replace("data/","")
+                            console.log(imageId);
+                            this.insertImage(imageId);
+                            showMessage("图片上传成功");
+                        } else {
+                            showMessage("图片上传失败");
+                        }
+                    } catch (error) {
+                        console.error('上传失败:', error);
+                        showMessage('图片上传失败');
+                    }
+                }
+            }
+        };
+        input.click();
     }
+
+    init(){
+        // this.plugin.addIcons(`
+        // <symbol id="iconImage" viewBox="0 0 512 512">
+            
+        // </symbol>
+        // `);
+        this.plugin.addTopBar({
+            icon: "iconImgDown",
+            title: "图片压缩",
+            position: "right",
+            callback: () => {
+                this.openFileDialog();
+            }
+        });
+
+    }
+
+    // 插入图片到编辑器
+    private insertImage(imageId: string) {
+        if (this.M_image_protyle) {
+            const imgMd = `![](${imageId})`;
+            this.M_image_protyle.getInstance().insert(imgMd);
+        }
+    }
+
+
 
     onLayoutReady(){
         this.plugin.eventBus.on("switch-protyle", async (event) => {
@@ -40,5 +102,4 @@ export class M_imageCompression {
             console.log("switch-image-protyle");
         });
     }
-
 }
