4104d26f6f6c14e248a5285f53bd4d60019e485d
loonghfut <165535596+loonghfut@users.noreply.github.com>
2025-02-25 21:10:33 +0800

    改进优先级逻辑

--- src/calendar/myF.ts
+++ src/calendar/myF.ts
@@ -629,7 +629,9 @@ export async function createEventInDatabase(//OK:加一个是否刷新日历的�
             const selectdata: ISelectOption[] = [{ content: status }];
             const priorityData: ISelectOption[] = [{ content: priority }];
             console.log("selectdata", selectdata);
-            await api.updateAttrViewCell_pro(id, to_db_id, priorityKeyID, priorityData, "select");
+            if (priority && priorityKeyID && priorityData) {
+                await api.updateAttrViewCell_pro(id, to_db_id, priorityKeyID, priorityData, "select");
+            }
             await api.updateAttrViewCell_pro(id, to_db_id, statusKeyID, selectdata, "select");
             await api.updateAttrViewCell_pro(id, to_db_id, checkboxKeyID, ismain, "checkbox");
             if (panel.isUploading()) {
