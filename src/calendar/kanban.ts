18fe1cdb20da5486077bc7b4e50558db5c53ff18
loonghfut <165535596+loonghfut@users.noreply.github.com>
2025-08-10 11:40:01 +0800

    看板：默认显示所有类别

--- src/calendar/kanban.ts
+++ src/calendar/kanban.ts
@@ -236,9 +236,9 @@ const CustomViewConfig = {
         const html = `
             <div class="kanban-container">
             <div class="kanban-board">
-            ${columns.todo.length ? createColumn('未完成', columns.todo, 'todo') : ''}
-            ${columns.inProgress.length ? createColumn('进行中', columns.inProgress, 'inProgress') : ''}
-            ${columns.done.length ? createColumn('完成', columns.done, 'done') : ''}
+            ${columns.todo.length ? createColumn('未完成', columns.todo, 'todo') : createColumn('未完成', columns.todo, 'todo')}
+            ${columns.inProgress.length ? createColumn('进行中', columns.inProgress, 'inProgress') : createColumn('进行中', columns.inProgress, 'inProgress')}
+            ${columns.done.length ? createColumn('完成', columns.done, 'done') : createColumn('完成', columns.done, 'done')}
             ${!columns.todo.length && !columns.inProgress.length && !columns.done.length ?
                 `
                 <div class="kanban-column-empty">无事件
