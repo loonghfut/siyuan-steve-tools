3a85cad2073780dfc6500fc7e433d35b71ba9873
loonghfut <165535596+loonghfut@users.noreply.github.com>
2025-09-24 20:18:39 +0800

    优化部分描述

--- src/settings/aggregate.ts
+++ src/settings/aggregate.ts
@@ -15,9 +15,9 @@ export const aggregateDefaults: Record<string, any> = {
 };
 
 export const aggregateGroup = (ctx: BuildContext): SettingGroupDefinition => ({
-    name: "🚧聚合🚧",
+    name: "聚合查询",
     items: [
-        { type: "checkbox", title: "启用聚合", description: "启用后再进行下面的设置", key: "aggregate-enable", value: ctx.settings["aggregate-enable"] },
+        { type: "checkbox", title: "启用聚合查询", description: "启用后再进行下面的设置", key: "aggregate-enable", value: ctx.settings["aggregate-enable"] },
         { type: "checkbox", title: "启用 SQL 可视化生成器", description: "在顶部栏显示 SQL 可视化生成器按钮", key: "aggregate-enable-sql-visualizer", value: ctx.settings["aggregate-enable-sql-visualizer"] },
         {
             type: "textarea",
